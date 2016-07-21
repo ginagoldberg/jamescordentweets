aaafe65d6b34a25fbaed8c1a5a58f50486f4efc9
Gina Goldberg <gina.goldberg@hotmail.com>
2016-07-22 01:27:35 +0200

    add methods in home controller that takes all tweets from the last six months and bypasses the 200 tweet limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,25 @@
 class HomeController < ApplicationController
 
   def index
-    @tweets = $twitter.user_timeline("matthijsMmaas", count: 10)
-    respond_to do |format|
-    format.html # index.html.erb
-    format.json { render json: @tweets }
+    @six_months_ago = Time.now - 6.months
+
+    @tweets = $twitter.get_all_tweets("JKCorden").select { |tweet| tweet.created_at >= @six_months_ago }
+
   end
 
-end
+  private
 
-private
+  def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = yield(max_id)
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
 
-  # def collect_with_max_id(collection=[], max_id=nil, &block)
-  #   response = yield(max_id)
-  #   collection += response
-  #   response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-  # end
-
-  # def get_all_tweets(user)
-  #   collect_with_max_id do |max_id|
-  #     options = {count: 200, include_rts: true}
-  #     options[:max_id] = max_id unless max_id.nil?
-  #     user_timeline(user, options)
-  #   end
-  # end
-
-
+  def $twitter.get_all_tweets(user)
+    collect_with_max_id do |max_id|
+      options = {count: 200, include_rts: true}
+      options[:max_id] = max_id unless max_id.nil?
+      user_timeline(user, options)
+    end
+  end
 end
